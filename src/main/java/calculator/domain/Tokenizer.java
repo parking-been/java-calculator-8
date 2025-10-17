5dc1053a9666998868c5bc689a335fa04d3e088e
parking-been <ysbeen00@gmail.com>
2025-10-17 14:36:52 +0900

    Feat(Tokenizer) : operand validation 구현 -숫자부분으로 이루어져 있는지 확인하고 → int 로 바꿔주기

--- src/main/java/calculator/domain/Tokenizer.java
+++ src/main/java/calculator/domain/Tokenizer.java
@@ -1,11 +1,15 @@
 package calculator.domain;
 
+import java.util.ArrayList;
+
+import calculator.exception.ErrorMessage;
 import net.bytebuddy.description.ByteCodeElement;
+import org.assertj.core.error.ErrorMessageFactory;
 
 public class Tokenizer {
     private String calculationString;
     private String delimiter;
-
+    private ArrayList<Integer> operand = new ArrayList<>();
     public Tokenizer(String calculationString, String delimiter){
         this.calculationString = calculationString;
         this.delimiter = delimiter;
@@ -21,7 +25,13 @@ public class Tokenizer {
         String[] parts = calculationString.split("[" + delimiter + "\\s]+");
 
         for (String ele : parts){
-            System.out.println(ele);
+            if(ele.matches("\\d+")){
+                operand.add(Integer.parseInt(ele));
+            }
+            else{
+                throw new IllegalArgumentException(ErrorMessage.TOKENIZER_INPUT_ERROR.getMessage());
+            }
         }
+
     }
 }
